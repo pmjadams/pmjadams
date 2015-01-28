ddcd66d7a789dd1c14c277abd5b5734eb443d87d
pmjadams <petera@netherhall.org.uk>
2015-01-28 16:01:36 +0000

    add a few comments, v 0.5

--- noughts/noughts.go
+++ noughts/noughts.go
@@ -1,3 +1,8 @@
+// The Game of Noughts and Crosses
+//
+//	Jan 2015
+//	  V 0.5
+
 package main
 
 import (
@@ -6,16 +11,23 @@ import (
 	"bufio"
 )
 
+//  The squares are numbered like this
+
 var board = [][]byte{
 	{'1', '2', '3'},
 	{'4', '5', '6'},
 	{'7', '8', '9'},
 }
+
+//  The moves are recorded here:  X,  0 or '.'
+
 var play = [][]byte{
 	{'.', '.', '.'},
 	{'.', '.', '.'},
 	{'.', '.', '.'},
 }
+
+//  Display the board, or the moves, on the screen
 
 func display(bd [][]byte) {
 	for x := 0; x < 3; x++ {
@@ -28,6 +40,8 @@ func display(bd [][]byte) {
 
 var move byte
 
+//  Ask the player for his or her next move
+
 func interact() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Printf("Enter your move (as a number between 1 and 9) : ")
@@ -38,10 +52,15 @@ func interact() {
 	if _, err := fmt.Sscanf(line, "%c", &move); err != nil {
 		fmt.Fprintln(os.Stderr, "invalid input")
 	}
-	fmt.Printf("\nThe move was: %c ", move)
+	if move <= '0' || move > '9' {
+	 fmt.Println("Invalid input. Please enter a number between 1 and 9, and press return")
+	}
+	fmt.Printf("\nThe move was: %d ", move - '0')
 	fmt.Println("")
 }
 
+
+//  Run the game
 
 func main() {
 

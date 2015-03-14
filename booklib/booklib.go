56361c9afce09bcfa26678a1efa863be93c13130
pmjadams <petera@netherhall.org.uk>
2015-03-14 18:40:09 +0000

    Ver. 0.4  Tidy up code
    Reads in data records from raw sql dump.

--- booklib/booklib.go
+++ booklib/booklib.go
@@ -3,260 +3,231 @@
 //
 //				Ver. 0.1	-	implement the basics, plus debug output
 //				Ver. 0.2	-	read in one record of data
-//				Ver. 0.3	-	modify SplitN, seems to work :-) 
+//				Ver. 0.3	-	modify SplitN, seems to work :-)
+//				Ver. 0.4	-	simplify layout, remove reps
 //
 //				ToDo: Marshall records to disk
-//						
+//
 
 package main
 
 import (
-		"fmt"
-		"bufio"
-		"os"
-		"strings"
-		"strconv"
-		"time"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // Book Record
-type  BookRec struct{
-	Book_Id 			int
-	Author				string
-	Title				string
-	Publisher			string
-	Pub_Place			string
-	Pub_Year			int
-	Cn_Type				string
-	Cn_Source			string
-	Cn_Item				int
-	Cn_Suffix			int
-	ISBN				string
-	ISSN				string
-	URL					string
-	Pages				string
-	Copies				int
-	Comment				string
-	Comment_Date		int64
-	Barcode				int
-	HomeLib				string
-	HoldingLib			string
-	Onloan				int64
-	DateLastBorrowed	int64
-	NotForLoan			bool
-	Damaged				bool
-	ItemLost			bool
-	Withdrawn			bool
-	Restricted			bool
-	ItemNotes			string
-	Issues				int
-	Renewals			int
-	Reserves			int
-	CopyNumber			int
-	Created_At			int64
+type BookRec struct {
+	Book_Id          int
+	Author           string
+	Title            string
+	Publisher        string
+	Pub_Place        string
+	Pub_Year         int
+	Cn_Type          string
+	Cn_Source        string
+	Cn_Item          int
+	Cn_Suffix        int
+	ISBN             string
+	ISSN             string
+	URL              string
+	Pages            string
+	Copies           int
+	Comment          string
+	Comment_Date     int64
+	Barcode          int
+	HomeLib          string
+	HoldingLib       string
+	Onloan           int64
+	DateLastBorrowed int64
+	NotForLoan       bool
+	Damaged          bool
+	ItemLost         bool
+	Withdrawn        bool
+	Restricted       bool
+	ItemNotes        string
+	Issues           int
+	Renewals         int
+	Reserves         int
+	CopyNumber       int
+	Created_At       int64
 }
-
-	
-
 
 // utility function
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 var lb = make([]BookRec, 5000)
-var j int	//  Indexes through lb records
+var j int //  Indexes through lb records
 var record BookRec
 
-func main () {
-  // Read
-  f, err := os.Open("books.sql")
-  check(err)
-// Define some counters and other temp vars
-	var i int	//	Indexes through fields in BookRec
-	var s string	// Temp place to hold a string
-	var k int	// Holds result of Atoi()
-// Set input source.
-    scanner := bufio.NewScanner(f)
-    // Set the split function for the scanning operation.
-    scanner.Split(bufio.ScanLines)
-    // Make a slice to hold all the book records
-    // Holder for a split string
-    var bits []string
-    // A Time holder
-    var tim time.Time
-    //
+func main() {
+	// Read
+	f, err := os.Open("books.sql")
+	check(err)
+	// Define some counters and other temp vars
+	var i int    //	Indexes through fields in BookRec
+	var s string // Temp place to hold a string
+	var k int    // Holds result of Atoi()
+	// Set input source.
+	scanner := bufio.NewScanner(f)
+	// Set the split function for the scanning operation.
+	scanner.Split(bufio.ScanLines)
+	// Make a slice to hold all the book records
+	// Holder for a split string
+	var bits []string
+	// A Time holder
+	var tim time.Time
+	//
 	parsing := true
 	j = 0
-// Loop begins here
+	// Loop begins here
 	for parsing {
 		// Create a record
 		record = BookRec{}
 		// Advance to next line, & validate the input
 		if scanner.Scan() == false {
-			break		// EOF, hopefully
+			break // EOF, hopefully
 		}
 		s = scanner.Text()
 		if s[0] != '(' {
-			continue		// Not a data record
+			continue // Not a data record
 		}
-		s = s[1:len(s)-2]	// remove enclosing ( )
-	
+		s = s[1 : len(s)-2] // remove enclosing ( )
+
 		bits = genSplit(s, ",", "'", 0, 33)
 		if err = scanner.Err(); err != nil {
-				fmt.Printf("Invalid input: %s", err)
+			fmt.Printf("Invalid input: %s", err)
+		}
+		// Trim for spaces
+		for index := range bits {
+			bits[index] = strings.TrimSpace(bits[index])
 		}
 		// Try parsing
 		i = 0
-	// Book_Id
+		// Book_Id
 		record.Book_Id, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-	// Author
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Author = strings.Trim(s, "'")	// 
+		// Author
+		record.Author = strings.Trim(s, "'")
 		i++
-	// Title
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Title = strings.Trim(s, "'")	// 
+		// Title
+		record.Title = strings.Trim(s, "'")
 		i++
-	// Publisher
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Publisher = strings.Trim(s, "'")	// 
+		// Publisher
+		record.Publisher = strings.Trim(s, "'")
 		i++
-	// Pub_Place
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Publisher = strings.Trim(s, "'")	// 
+		// Pub_Place
+		record.Pub_Place = strings.Trim(s, "'")
 		i++
-	// Pub_Year
-		s = strings.TrimSpace(bits[i])
-		record.Pub_Year, err = strconv.Atoi(s)
+		// Pub_Year
+		record.Pub_Year, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-	// Cn_Type
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Cn_Type = strings.Trim(s, "'")	// 
+		// Cn_Type
+		record.Cn_Type = strings.Trim(bits[i], "'")
 		i++
-	// Cn_Source
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Cn_Source = strings.Trim(s, "'")	// 
+		// Cn_Source
+		record.Cn_Source = strings.Trim(bits[i], "'")
 		i++
-	// Cn_Item
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Cn_Item, err = strconv.Atoi(s)
+		// Cn_Item
+		record.Cn_Item, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-	// Cn_Suffix
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Cn_Suffix, err = strconv.Atoi(s)
+		// Cn_Suffix
+		record.Cn_Suffix, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-
-	// ISBN
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.ISBN = strings.Trim(s, "'")	// 
+		// ISBN
+		record.ISBN = strings.Trim(bits[i], "'")
 		i++
-	// ISSN
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.ISSN = strings.Trim(s, "'")	// 
+		// ISSN
+		record.ISSN = strings.Trim(bits[i], "'")
 		i++
-	// URL
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.URL = strings.Trim(s, "'")	// 
+		// URL
+		record.URL = strings.Trim(bits[i], "'")
 		i++
-	// Pages
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Pages = strings.Trim(s, "'")	// 
+		// Pages (as a string)
+		record.Pages = strings.Trim(bits[i], "'")
 		i++
-	// Copies
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Copies, err = strconv.Atoi(s)
+		// Copies
+		record.Copies, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-	// Comment
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Comment = strings.Trim(s, "'")	// 
+		// Comment
+		record.Comment = strings.Trim(bits[i], "'")
 		i++
-	// Comment_Date
-		record.Comment_Date = 0		//Ignore data input
+		// Comment_Date
+		record.Comment_Date = 0
+		i++ //Ignore data input
+		// Barcode
+		record.Barcode, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-
-	// Barcode
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.Barcode, err = strconv.Atoi(s)
-		check(err)
+		// HomeLib
+		record.HomeLib = strings.Trim(bits[i], "'")
 		i++
-	// HomeLib
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.HomeLib = strings.Trim(s, "'")	// 
+		// HoldingLib
+		record.HoldingLib = strings.Trim(bits[i], "'")
 		i++
-	// HoldingLib
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.HoldingLib = strings.Trim(s, "'")	// 
-		i++
-	// Onloan
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		s = strings.Trim(s, "'")
+		// Onloan
+		s = strings.Trim(bits[i], "'")
 		if s == "NULL" {
 			record.Onloan = 0
 		} else {
-			s += " 00:00:00"
-			tim, err = time.Parse("2006-01-02 15:04:05", s)
+			tim, err = time.Parse("2006-01-02", s)
 			check(err)
 			record.Onloan = tim.Unix()
 		}
 		i++
-	// DateLastBorrowed
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		s = strings.Trim(s, "'")
+		// DateLastBorrowed
+		s = strings.Trim(bits[i], "'")
 		if s == "NULL" {
 			record.DateLastBorrowed = 0
 		} else {
-			s += " 00:00:00"
-			tim, err = time.Parse("2006-01-02 15:04:05", s)
+			tim, err = time.Parse("2006-01-02", s)
 			check(err)
 			record.DateLastBorrowed = tim.Unix()
 		}
 		i++
-	// NotForLoan
-		s = strings.TrimSpace(bits[i])
-		k, err = strconv.Atoi(s)
+		// NotForLoan
+		k, err = strconv.Atoi(bits[i])
 		record.NotForLoan = (k == 1)
 		check(err)
 		i++
-	// Damaged
-		s = strings.TrimSpace(bits[i])
-		k, err = strconv.Atoi(s)
+		// Damaged
+		k, err = strconv.Atoi(bits[i])
 		record.Damaged = (k == 1)
 		check(err)
 		i++
-	// ItemLost
-		s = strings.TrimSpace(bits[i])
-		k, err = strconv.Atoi(s)
+		// ItemLost
+		k, err = strconv.Atoi(bits[i])
 		record.ItemLost = (k == 1)
 		check(err)
 		i++
-	// Withdrawn
-		s = strings.TrimSpace(bits[i])
-		k, err = strconv.Atoi(s)
+		// Withdrawn
+		k, err = strconv.Atoi(bits[i])
 		record.Withdrawn = (k == 1)
 		check(err)
 		i++
-	// Restricted
-		s = strings.TrimSpace(bits[i])
-		k, err = strconv.Atoi(s)
+		// Restricted
+		k, err = strconv.Atoi(bits[i])
 		record.Restricted = (k == 1)
 		check(err)
 		i++
-	// ItemNotes
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.ItemNotes = strings.Trim(s, "'")	// 
+		// ItemNotes
+		record.ItemNotes = strings.Trim(bits[i], "'")
 		i++
-	// Issues
-		s = strings.TrimSpace(bits[i])		// Trim leading space
+		// Issues
+		s = bits[i]
 		if s == "NULL" {
 			record.Issues = 0
 		} else {
@@ -265,8 +236,8 @@ func main () {
 			record.Issues = k
 		}
 		i++
-	// Renewals
-		s = strings.TrimSpace(bits[i])		// Trim leading space
+		// Renewals
+		s = bits[i]
 		if s == "NULL" {
 			record.Renewals = 0
 		} else {
@@ -275,8 +246,8 @@ func main () {
 			record.Renewals = k
 		}
 		i++
-	// Reserves
-		s = strings.TrimSpace(bits[i])		// Trim leading space
+		// Reserves
+		s = bits[i]
 		if s == "NULL" {
 			record.Reserves = 0
 		} else {
@@ -285,18 +256,16 @@ func main () {
 			record.Reserves = k
 		}
 		i++
-	// CopyNumber
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		record.CopyNumber, err = strconv.Atoi(s)
+		// CopyNumber
+		record.CopyNumber, err = strconv.Atoi(bits[i])
 		check(err)
 		i++
-	// Created_At
-		s = strings.TrimSpace(bits[i])		// Trim leading space
-		s = strings.Trim(s, "'")
+		// Created_At
+		s = strings.Trim(bits[i], "'")
 		tim, err = time.Parse("2006-01-02 15:04:05", s)
 		check(err)
 		record.Created_At = tim.Unix()
-	// We are finished
+		// We are finished
 		lb[j] = record
 		j++
 		if j == 4999 {
@@ -304,7 +273,6 @@ func main () {
 		}
 	}
 }
-
 
 // Generic split (from strings std lib), modified: splits after each instance of sep,
 // including sepSave bytes of sep in the subarrays. Does not
@@ -323,7 +291,7 @@ func genSplit(s, sep, quote string, sepSave, n int) []string {
 	//if n < 0 {
 	//	n = Count(s, sep) + 1
 	//}
-	toggle := false		// Toggle when quote char seen
+	toggle := false // Toggle when quote char seen
 	c := sep[0]
 	q := quote[0]
 	start := 0
@@ -343,4 +311,3 @@ func genSplit(s, sep, quote string, sepSave, n int) []string {
 	a[na] = s[start:]
 	return a[0 : na+1]
 }
-
